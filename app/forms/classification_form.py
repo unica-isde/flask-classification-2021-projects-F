e4d6949b69a89188ce37581077e1d1f4cb50ea8b
ale <alex0697@ymail.com>
2022-03-11 11:19:08 +0100

    added forms, no validation

--- app/forms/classification_form.py
+++ app/forms/classification_form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, SelectField
+from wtforms import SubmitField, SelectField, FloatField
 from wtforms.validators import DataRequired
+
 
 from app.utils.list_images import list_images
 from config import Configuration
@@ -11,4 +12,9 @@ conf = Configuration()
 class ClassificationForm(FlaskForm):
     model = SelectField('model', choices=conf.models, validators=[DataRequired()])
     image = SelectField('image', choices=list_images(), validators=[DataRequired()])
+    brightness = FloatField('brightness')
+    saturation = FloatField('saturation')
+    contrast = FloatField('contrast')
+    hue = FloatField('hue')
+
     submit = SubmitField('Submit')
