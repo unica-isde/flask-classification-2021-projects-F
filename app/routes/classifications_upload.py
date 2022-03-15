2ddcd3fdba08c0a2afaf9b9d3d072d67f3a7e869
Matteo Asuni <asunimatteo@gmail.com>
2022-03-15 18:31:09 +0100

    Implementation of the api for uploading the image Issue #4

--- app/routes/classifications_upload.py
+++ app/routes/classifications_upload.py
@@ -1,14 +1,21 @@
-from flask import request, render_template
+from flask import request, render_template, redirect
 from app import app
 from app.forms.classification_form import ClassificationForm
+from ..utils.image_upload import check_upload, save_image
 
 
 @app.route('/classifications_upload', methods=['GET', 'POST'])
 def classifications_upload():
-    
     form = ClassificationForm()
     if request.method == "POST":
-       # TODO  
-       pass
+        # check if the form has the image field
+        if 'uploaded_img' not in request.files:
+            return redirect(request.url)
+        image = request.files['uploaded_img']
+        if not check_upload(image):
+            return redirect(request.url)
+        # saving the image
+        filename, img_folder = save_image(image)
+
 
     return render_template('classifications_upload.html', form=form)
