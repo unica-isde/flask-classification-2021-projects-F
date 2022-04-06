2abf3c404b84a9428186129fd86c22cc80a41055
Matteo Asuni <asunimatteo@gmail.com>
2022-04-06 21:58:25 +0200

    Fixed comments in transform_utils

--- ml/transform_utils.py
+++ ml/transform_utils.py
@@ -8,17 +8,18 @@ from config import Configuration
 conf = Configuration()
 
 def transform_image(img_id, brightness_id, contrast_id, saturation_id, hue_id):
-    """Returns the top-5 classification score output from the
-    model specified in model_id when it is fed with the
-    image corresponding to img_id."""
+    """Returns the image with the corresponding transformations (choosen by the user) applied"""
+    
+    #fetching the image
     img = fetch_image(img_id)
 
     try:
-        os.mkdir(conf.transformed_folder_path)
-    except FileExistsError:
+        os.mkdir(conf.transformed_folder_path) #we try to create the output folder
+    except FileExistsError: #if the folder already exists we remove the previous output files
         for f in os.listdir(conf.transformed_folder_path):
             os.remove(os.path.join(conf.transformed_folder_path, f))
 
+    #creation of the transform object that combines all the transformations that we want to apply to the image
     transform = transforms.Compose(
         [
             transforms.ToTensor(),
@@ -37,9 +38,11 @@ def transform_image(img_id, brightness_id, contrast_id, saturation_id, hue_id):
     preprocessed = transform(img)
 
     img.close()
-
+    
+    #retrieving current time to create an unique filename
     now = time.time_ns()
     img_name = str(now) + '.JPEG'
+    #saving the image to the ouput folder
     output_path = os.path.join(conf.transformed_folder_path, img_name)
     torchvision.utils.save_image(preprocessed, output_path)
 
